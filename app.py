8d619978ed9d2a011581672ab912a8a5fe309c6a
AdamKuzara <ada2163629@maricopa.edu>
2024-11-19 16:57:16 -0700

    Update app.py
    Created the login form. Next is using Bcrypt

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_word():
-    return "Hello, World!"
+@app.route('/', methods=['GET'])
+def login_form():
+    form_html = """
+     <form method="POST" action="/login">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <input type="submit" value="Login">
+    </form>
+    """
+    return render_template_string(form_html)
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    return f"Username: {username}, Password: {password}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
