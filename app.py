6547cddb2d828cfc15375874ecc1ae25cfe0d8cc
AdamKuzara <ada2163629@maricopa.edu>
2024-11-19 19:05:24 -0700

    Update app.py
    A lot of changes I forgot to commit incrementally here. I added everything to the login function specifically the input validation for the password and hashing the password.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template_string
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 @app.route('/', methods=['GET'])
 def login_form():
@@ -19,8 +21,23 @@ def login_form():
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    return f"Username: {username}, Password: {password}"
-
+    # If statements for input validation.
+    if len(password) < 8:
+        return "Invalid password. Must be at least 8 characters long."
+    if not any(char.isalpha() for char in password):
+        return "Invalid password. Must include letters."
+    if not any(char.isdigit() for char in password):
+        return "Invalid password. Must include numbers."
+    # Encrypted password is stored - I tried to use generate_password_hash but I kept getting an error.
+    # I looked up a different way to do salting (https://heynode.com/blog/2020-04/salt-and-hash-passwords-bcrypt) but decided not to try and do it.
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    # Feedback for validation when comparing the plain-text password to the hashed/encrypted password
+    if bcrypt.check_password_hash(hashed_password, password):
+        validation_message = "Password is valid!"
+    else:
+        validation_message = "Invalid password."
+    return (f"Username: {username}, Password: {password}<br>"
+            f"{validation_message}")
 
 if __name__ == "__main__":
     app.run(debug=True)
